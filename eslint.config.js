864e323b14a0196181dc5a1810d531d03974465a
Parikshit-GH17 <parikshiturkande123@gmail.com>
2024-12-09 19:55:18 +0530

    configure auto import sorting for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { sortUserPlugins } from 'vite'
 
 export default [
   { ignores: ['dist'] },
@@ -21,9 +22,10 @@ export default [
     plugins: {
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh, 'simple-import-sort'
     },
     rules: {
+    'simple-import-sort/imports': 'error',
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { elements } from 'chart.js'
+import {Routes} from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>     LMS
+    <>    
+    <Routes>
+    { /*<Route path="/" element={<Home />}  ></Route> */}
+      </Routes> 
+  
     </>
   )
 }
